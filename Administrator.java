29b44d4a26e46a220585fc4d6d8c4e5e9852a932
xmalagon-ctrl <xmalagon@unal.edu.co>
2026-04-24 11:27:19 -0500

    Add Administrator class extending User

---
+++ Administrator.java
@@ -0,0 +1,11 @@
+//clase hija para administraador
+public class Administrator extends User{
+    //atributos
+    //..
+
+    //constructor
+    public Administrator (String username, int cedula){
+        super(username, cedula);
+    }
+
+}
